d05f7aad89061201876de332d6184c50609738a0
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-04-27 17:38:50 +0900

    finish

--- app.py
+++ app.py
@@ -36,15 +36,16 @@ def transform_img(img):
 
 def deal_rotation(img):
     """postした画像が回転してしまった場合，元に戻す"""
+    
     for orientation in ExifTags.TAGS.keys() : 
-                if ExifTags.TAGS[orientation]=='Orientation' : break 
-            exif=dict(img._getexif().items())
-            if   exif[orientation] == 3 : 
-                img=img.rotate(180, expand=True)
-            elif exif[orientation] == 6 : 
-                img=img.rotate(270, expand=True)
-            elif exif[orientation] == 8 : 
-                img=img.rotate(90, expand=True)
+        if ExifTags.TAGS[orientation]=='Orientation' : break 
+    exif=dict(img._getexif().items())
+    if   exif[orientation] == 3 : 
+        img=img.rotate(180, expand=True)
+    elif exif[orientation] == 6 : 
+        img=img.rotate(270, expand=True)
+    elif exif[orientation] == 8 : 
+        img=img.rotate(90, expand=True)
     return img
 
 @app.route("/predict", methods=["POST"])
