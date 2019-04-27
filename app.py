7431db469e012ab0207e91371a78b3e9f5ff68ef
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-04-27 14:29:52 +0900

    complete the objective

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from keras.models import load_model
 import flask
-import numpy as numpy
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from keras import backend as K
 
 app = flask.Flask(__name__)
-model = None
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
 
 def load():
-    global model
-    model = load_model("mnist.h5")
+    model = load_model("mnist.h5", compile=False)
+    return model
+
 
 @app.route("/", methods = ["GET"])
 def help():
@@ -23,20 +26,27 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-# @app.route("/", methods=["POST"])
-# def predict():
-#     response = {"Content-Type": "application/json",
-#                 "result": None, 
-#                 "probability": None}
-#     if flask.request.method == "POST":
-#         if flask.request.files["file"] and allowed_file(flask.request.files["file"]):   #exist file
-#             im = np.array(Image.open(flask.request.files["file"]))
-#             return im
-#     # return flask.request.files["file"]
-#     return flask.jsonify(response)
+@app.route("/", methods=["POST"])
+def predict():
+    model = load()
+    response = {"Content-Type": "application/json",
+                "result": None, 
+                "probability": None}
+    if flask.request.method == "POST":
+        if flask.request.files["file"]:
+            img = np.array(Image.open(flask.request.files["file"]).convert('L'))
+            width,height = 28, 28
+            img = np.resize(img, (width,height))
+            img = img.reshape((1, width, height, 1))
+            img = img.astype('float32')
+            img /= 255
+            result = model.predict(img,verbose=0)
+            K.clear_session()
+            response["result"] = str(np.argmax(result))
+            response["probability"] = str(np.max(result))
+    return flask.jsonify(response)
             
 
 
 if __name__ == '__main__':
-    load()
     app.run()
